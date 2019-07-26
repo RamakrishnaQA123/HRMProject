004474e6b76dedec154d25116f3fec8ffe794422
LENOVO <LENOVO@192.168.10.115>
2019-07-26 12:49:59 +0530

    Added SampleTC123.class

---
+++ SeleniumProject/src/Javaprogrames/SampleTC123.java
@@ -0,0 +1,15 @@
+package Javaprogrames;
+
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class SampleTC123 {
+
+	public static void main(String[] args) {
+
+		
+		FirefoxDriver driver=new FirefoxDriver();
+		driver.get("http://seleniumhq.org");
+		
+	}
+
+}
